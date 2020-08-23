9b8360ef80f910921d64a0cf0954ed6ccf3d0b2e
Shakeeb Ahmed <Shakeebazaheer@gmail.com>
2020-08-23 05:34:09 +0000

    initial hashtables

---
+++ H_HashTables.py
@@ -0,0 +1,11 @@
+#Hash Table is basically a dictionary in python
+myDict = {"shakeeb":1 , "Awais":2}
+print(myDict)
+print(myDict["shakeeb"]) #O(1)
+
+#>> md5 generator http://www.miraclesalad.com/webtools/md5.php
+#>> hashtables have collisions and becomes O(n/k) k= size of the hashtable
+#>> https://www.cs.usfca.edu/~galles/visualization/OpenHash.html
+#>> https://en.wikipedia.org/wiki/Hash_table
+
+
